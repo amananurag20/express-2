19bf6cec72c8b96d1cfcd53788353f869512640d
amananurag <amananurag.20@gmail.com>
2025-05-28 15:05:30 +0530

    header based auth

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,7 +4,7 @@ const jwt= require("jsonwebtoken")
 const verifyToken=(req,res,next)=>{
   
     const token=req.headers.authorization.split(" ")[1];
-  //check token
+  //check token-
     try{
         const userData=jwt.verify(token,"hello");
         req.user=userData;
